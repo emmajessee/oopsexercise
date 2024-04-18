afbc9cf813c18db35e2af1c794d95ddb4f90f436
emmajessee <155660845+emmajessee@users.noreply.github.com>
2024-04-18 10:40:38 -0500

    I forgot to save oh my gosh

--- script.js
+++ script.js
@@ -53,3 +53,49 @@ const mulan1 = new disneymovie('Mulan')
 const tangled1= new disneymovie('Tangled')
 
 //4a. 
+class DM {
+    constructor(t, m){
+        this.title = t;
+        this.main =m;
+    }
+    static loveDisneyMovies (){
+    return 'I love Disney Movies!!!!'
+}
+}
+//4b.
+DM.prototype.storyline = function() {
+return 'The movie ${this.title} is about ${this.main}'
+}
+
+//4c. 
+const mulan2 = new DM('Mulan','Fa Mulan')
+
+//4d. 
+const tangled2 = new DM('Tangled', 'Rapunzel')
+
+//4e. 
+//in 4a. 
+
+//4f. 
+console.log(DM.loveDisneyMovies());
+
+//5a. 
+class DMcast extends DM {
+    constructor(t, m, c){
+        super(t,m),
+        this.cast = c
+
+    }
+}
+
+//5b.
+//in 5a
+
+//5c. 
+//in 5a 
+
+//5d.
+const mulan3 = new DMcast('Mulan','Fa Mulan',{mulan: 'Wing Na Wen', mushu: 'Eddie Murphy', shang: 'BD Wong', theemperor: 'Pat Morita'})
+
+//5e. 
+const rapunzel3 = new DMcast('Tangled','Rapunzel',{rapunzel: 'Mandy Moore',flynnryder:'Zachary levi',mothergotel:'Donna Murphy'})
